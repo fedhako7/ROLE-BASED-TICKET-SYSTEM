67f6222a27a1250746cbcb6a0b383fee39aa7572
Fedesa Yelmachew <fedhasayelmachew@gmail.com>
2025-03-04 10:46:47 +0300

    Added ticket getting endpoint

--- backend/controllers/ticketControllers/ticketControllers.js
+++ backend/controllers/ticketControllers/ticketControllers.js
@@ -1,6 +1,6 @@
 const Ticket = require("../../models/ticketModels/ticketModels");
-const status_codes = require("http-status-codes")
-
+const status_codes = require("http-status-codes");
+const roles = require("../roles/roles");
 
 
 const createTicketController = async (req, res) => {
@@ -24,7 +24,21 @@ const createTicketController = async (req, res) => {
 
 
 const getTicketController = async (req, res) => {
-}
+  try {
+    let tickets;
+
+    if (req.user.role === roles.ADMIN) {
+      tickets = await Ticket.find().populate("user_id", "username fname lname");
+    } else if (req.user.role === roles.USER) {
+      tickets = await Ticket.find({ user_id: req.user.userId }).populate("user_id", "username fname lname");
+    }
+
+    res.status(status_codes.OK).json({ tickets });
+  } catch (error) {
+    console.error(error);
+    res.status(status_codes.INTERNAL_SERVER_ERROR).json({ message: "Server error" });
+  }
+};
 
 
 const updateTicketController = async (req, res) => {
