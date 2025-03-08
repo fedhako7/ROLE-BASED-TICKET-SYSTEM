e1ddfad8152dad5d7066f8fe6538158928417ff9
Fedesa Yelmachew <fedhasayelmachew@gmail.com>
2025-03-08 10:19:49 +0300

    Changed host for backend deployment

--- frontend/ticket system/src/api/api.js
+++ frontend/ticket system/src/api/api.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000',
+  // baseURL: 'http://localhost:3000',
   // baseURL: import.meta.env.VITE_BASE_URL,
+  baseURL: import.meta.env.VITE_BASE_URL_RENDER,
   
   headers: {
     'Content-Type': 'application/json',
